4b8844c520df9a7bd5b250a2b799df29f6b5d94d
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-05-03 03:15:37 +0300

    fdsfsd

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ impl Fairing for Cors {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
-            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+            "POST, PATCH, PUT, DELETE, HEAD, GET",
         ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
