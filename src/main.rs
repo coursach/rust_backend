47754373e2d457a017e59090cee4f7b0c9ffb702
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-05-06 07:33:06 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ impl Fairing for Cors {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:8000"));
+        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:3000"));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
             "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
